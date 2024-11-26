5b8abb1ad8ae17c29be5d85998f671e21aa79a60
Kevin Ruangrad <kevin_9@live.se>
2024-11-26 09:07:29 +0100

    last

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-const rollBtn = document.querySelector("#rollBtn");
-const freezeBtn = document.querySelector("#freezeBtn");
+const form = document.getElementById("form");
+const playerNameInput = document.getElementById("playerName");
+const game = document.querySelector(".game");
+const submitBtn = document.getElementById("submitBtn");
+const rollBtn = document.getElementById("rollBtn");
+const freezeBtn = document.getElementById("freezeBtn");
+const playerScoreEl = document.getElementById("playerScore");
+const roundCountEl = document.getElementById("roundCount");
+const totalScoreEl = document.getElementById("totalScore");
+const playerDisplay = document.getElementById("playerDisplay");
 const rollAmount = document.querySelector(".rollAmount");
-const playerScoreEl = document.querySelector("#playerScore");
-const roundCountEl = document.querySelector("#roundCount");
-const totalScoreEl = document.querySelector("#totalScore");
 
+submitBtn.addEventListener("click", startGame);
 rollBtn.addEventListener("click", rollDice);
 freezeBtn.addEventListener("click", freezeScore);
 
@@ -15,6 +21,13 @@ let totalScore = 0;
 roundCountEl.textContent = "Current round: " + roundCount;
 playerScoreEl.textContent = "Current score: " + playerScore;
 totalScoreEl.textContent = "Total score: " + totalScore;
+
+function startGame() {
+  const playerName = playerNameInput.value;
+  playerDisplay.textContent = playerName;
+  form.remove("form");
+  game.style.visibility = "visible";
+}
 
 function rollDice() {
   const randomNumber = Math.floor(Math.random() * 6) + 1;
@@ -38,7 +51,7 @@ function freezeScore() {
   playerScoreEl.textContent = "Current score: " + playerScore;
   roundCountEl.textContent = "Current round: " + roundCount;
   if (totalScore >= 100) {
-    alert("You won the game! " + ' Amount of rounds: ' + roundCount);
+    alert("You won the game! " + " Amount of rounds: " + roundCount);
     totalScore = 0;
   }
 }
